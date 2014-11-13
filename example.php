619bd96b0d0426fc3f92be4cdb1c0de0613cafb6
Kakhaber Kashmadze <kakhavk@gmail.com>
2014-11-13 17:19:04 +0400

    Update example.php

--- example.php
+++ example.php
@@ -10,6 +10,7 @@ define('DBNAME','databasename');
 $dbType="mysql";
 $osType="linux";
 $backupPath="/tmp/";
+/* For example in MS Windows backup path must be C:\Windows\Temp, or other path where is located temporary files */
 
 
 $backupCls->setDbType($dbType);

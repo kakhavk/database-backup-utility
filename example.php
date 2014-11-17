42a8250fb415cdc32e7985e9e601266be416a4da
Kakhaber Kashmadze <kakhavk@gmail.com>
2014-11-17 16:42:51 +0400

    Update example.php

--- example.php
+++ example.php
@@ -1,10 +1,10 @@
 <?php
 ini_set("display_errors", 1);
-require_once "Backup.php";
-$backupCls=new Backup();
+require_once "SqlDump.php";
+$sqlDumpCls=new Backup();
 
 $dbParams=array("localhost","username","password","databasename");
-$backupCls->setDbParams($dbParams);
-$backupCls->setDbType("MySQL");
-$backupCls->save("/tmp/"); /* For MS Windows backup path maybe C:\Windows\Temp, or other path where is located temporary files */
+$sqlDumpCls->setDbParams($dbParams);
+$sqlDumpCls->setDbType("MySQL");
+$sqlDumpCls->save("/tmp/"); /* For MS Windows backup path maybe C:\Windows\Temp, or other path where is located temporary files */
 ?>
